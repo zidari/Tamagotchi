194997fa60cf46df1e092ce43775d77c35b6d1b5
장향미 <hhh111gidal@naver.com>
2023-05-10 15:55:21 +0900

    미니프로젝트(저장)

---
+++ animal_1st/src/tamagochi/RankVo.java
@@ -0,0 +1,47 @@
+package tamagochi;
+
+public class RankVo {
+	private String id;
+	private int lv;
+	private int ra;
+
+
+	public RankVo(int ra, String id, int lv) {
+	   this.id = id;
+	   this.lv = lv;
+	   this.ra = ra;
+	   
+	}
+	
+	
+
+	public int getRa() {
+		return ra;
+	}
+
+
+
+	public void setRa(int ra) {
+		this.ra = ra;
+	}
+
+
+
+	public String getId() {
+	   return id;
+	}
+
+	public void setId(String id) {
+	   this.id = id;
+	}
+
+	public int getLv() {
+	   return lv;
+	}
+
+	public void setLv(int lv) {
+	   this.lv = lv;
+	}
+
+
+}
